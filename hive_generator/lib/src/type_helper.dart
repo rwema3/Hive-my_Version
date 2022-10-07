409e0ccc9d696f09b9601292f133d0f3cb30b4c1
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:33:04 -0700

    Update type_helper.dart

--- hive_generator/lib/src/type_helper.dart
+++ hive_generator/lib/src/type_helper.dart
@@ -10,12 +10,3 @@
 String revivableToString(DartObject? object, List<String> typeInformation) {
   final reader = ConstantReader(object);
   final revivable = reader.revive();
-
-  if (revivable.source.fragment.isEmpty) {
-    // Enums
-    return revivable.accessor;
-  } else {
-    // Classes
-    final nextTypeInformation = [...typeInformation, '$object'];
-    final prefix = kConstConstructors ? 'const ' : '';
-    final ctor = revivable.accessor.isEmpty ? '' : '.${revivable.accessor}';
