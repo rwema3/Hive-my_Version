24eeea16fcf0dfb5f824f3196e8400c2bf6e1478
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:40:44 -0700

    parm

--- hive_generator/lib/src/class_builder.dart
+++ hive_generator/lib/src/class_builder.dart
@@ -10,6 +10,16 @@
     if (constr!.parameters.isEmpty && fields.isEmpty) {
       return 'return ${cls.name}();';
     }
+
+
+    for (var param in constr.parameters) {
+      var field = fields.firstOrNullWhere((it) => it.name == param.name);
+      // Final fields
+      field ??= getters.firstOrNullWhere((it) => it.name == param.name);
+      if (field != null) {
+        if (param.isNamed) {
+          code.write('${param.name}: ');
+        }
         code.write(_value(
           param.type,
           'fields[${field.index}]',
