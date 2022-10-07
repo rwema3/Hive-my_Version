319534194fa9af55f80b5510f6ba7760b00f04f8
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:44:38 -0700

    throwUnsupported

--- hive_generator/lib/src/type_helper.dart
+++ hive_generator/lib/src/type_helper.dart
@@ -120,3 +120,5 @@ String literalToString(DartObject object, List<String> typeInformation) {
   );
 }
 
+Never throwUnsupported(String message) =>
+    throw InvalidGenerationSourceError('Error with `@HiveField`. $message');
