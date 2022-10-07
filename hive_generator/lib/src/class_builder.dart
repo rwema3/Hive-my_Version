b5bd40114cd5dc9427be73648ac141f05f9533a0
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:34:10 -0700

    Update class_builder.dart

--- hive_generator/lib/src/class_builder.dart
+++ hive_generator/lib/src/class_builder.dart
@@ -1,16 +1,4 @@
 
-/// Suffix to use when accessing a field in [type].
-/// $variable$suffix.field
-String _accessorSuffixFromType(DartType type) {
-  if (type.nullabilitySuffix == NullabilitySuffix.star) {
-    return '?';
-  }
-  if (type.nullabilitySuffix == NullabilitySuffix.question) {
-    return '?';
-  }
-  return '';
-}
-
 /// Suffix to use when casting a value to [type].
 /// $variable as $type$suffix
 String _suffixFromType(DartType type) {
