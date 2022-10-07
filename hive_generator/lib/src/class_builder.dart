a356a5d43a16e13f6ecb255d6e83cc67e4a23e94
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:34:13 -0700

    Update class_builder.dart

--- hive_generator/lib/src/class_builder.dart
+++ hive_generator/lib/src/class_builder.dart
@@ -1,16 +1,4 @@
 
-/// Suffix to use when casting a value to [type].
-/// $variable as $type$suffix
-String _suffixFromType(DartType type) {
-  if (type.nullabilitySuffix == NullabilitySuffix.star) {
-    return '';
-  }
-  if (type.nullabilitySuffix == NullabilitySuffix.question) {
-    return '?';
-  }
-  return '';
-}
-
 String _displayString(DartType e) {
   var suffix = _suffixFromType(e);
   return '${e.getDisplayString(withNullability: false)}$suffix';
