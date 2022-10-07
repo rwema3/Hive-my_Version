ca287a02bdf96cd00a8e50af7132f2c37524d7c6
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:33:28 -0700

    Update type_adapter_generator.dart

--- hive_generator/lib/src/type_adapter_generator.dart
+++ hive_generator/lib/src/type_adapter_generator.dart
@@ -93,6 +93,3 @@ class TypeAdapterGenerator extends GeneratorForAnnotation<HiveType> {
     return accessorNames;
   }
 
-  List<List<AdapterField>> getAccessors(
-      ClassElement cls, LibraryElement library) {
-    var accessorNames = getAllAccessorNames(cls);
