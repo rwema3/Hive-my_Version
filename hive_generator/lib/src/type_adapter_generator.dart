46fa22f22e202692db87dd3be796a139646f674a
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:45:55 -0700

    enums

--- hive_generator/lib/src/type_adapter_generator.dart
+++ hive_generator/lib/src/type_adapter_generator.dart
@@ -3,3 +3,4 @@ import 'package:build/build.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_generator/src/builder.dart';
 import 'package:hive_generator/src/class_builder.dart';
+import 'package:hive_generator/src/enum_builder.dart';
