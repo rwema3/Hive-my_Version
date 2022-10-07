1191e118405ad2d8d7ab6d21e2b103b657315e05
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:41:46 -0700

    reader isLiteral

--- hive_generator/lib/src/type_helper.dart
+++ hive_generator/lib/src/type_helper.dart
@@ -2,3 +2,8 @@
 ]) {
   if (object == null || object.isNull) return 'null';
   final reader = ConstantReader(object);
+  return reader.isLiteral
+      ? literalToString(object, typeInformation)
+      : revivableToString(object, typeInformation);
+}
+
