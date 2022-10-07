ce228cd0f25e62447bb066846dc6d5c8bc9c9d05
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:33:26 -0700

    Update type_adapter_generator.dart

--- hive_generator/lib/src/type_adapter_generator.dart
+++ hive_generator/lib/src/type_adapter_generator.dart
@@ -96,12 +96,3 @@ class TypeAdapterGenerator extends GeneratorForAnnotation<HiveType> {
   List<List<AdapterField>> getAccessors(
       ClassElement cls, LibraryElement library) {
     var accessorNames = getAllAccessorNames(cls);
-
-  int getTypeId(ConstantReader annotation) {
-    check(
-      !annotation.read('typeId').isNull,
-      'You have to provide a non-null typeId.',
-    );
-    return annotation.read('typeId').intValue;
-  }
-}
