dd90ab73d259cc5cf9eebd047c1f1f3b09ec0b64
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:46:12 -0700

    source gens imports

--- hive_generator/lib/src/type_adapter_generator.dart
+++ hive_generator/lib/src/type_adapter_generator.dart
@@ -5,3 +5,5 @@ import 'package:hive_generator/src/builder.dart';
 import 'package:hive_generator/src/class_builder.dart';
 import 'package:hive_generator/src/enum_builder.dart';
 import 'package:hive_generator/src/helper.dart';
+import 'package:source_gen/source_gen.dart';
+
