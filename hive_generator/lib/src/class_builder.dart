a5db575ceb55daef225687b4d2dcf1ea85fd130c
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-06 20:37:42 -0700

    type.nullabilitySuffix

--- hive_generator/lib/src/class_builder.dart
+++ hive_generator/lib/src/class_builder.dart
@@ -1,4 +1,16 @@
 
+/// Suffix to use when accessing a field in [type].
+/// $variable$suffix.field
+String _accessorSuffixFromType(DartType type) {
+  if (type.nullabilitySuffix == NullabilitySuffix.star) {
+    return '?';
+  }
+  if (type.nullabilitySuffix == NullabilitySuffix.question) {
+    return '?';
+  }
+  return '';
+}
+
 /// Suffix to use when casting a value to [type].
 /// $variable as $type$suffix
 String _suffixFromType(DartType type) {
