158d3ee6dbe7753d6ef92b6b83388579f4337d82
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-07 01:20:37 +0200

    GenerateMocks

--- hive_flutter/test/mocks.dart
+++ hive_flutter/test/mocks.dart
@@ -1,4 +1,9 @@
 library hive_flutter.test.mocks;
 
+
+@GenerateMocks([], customMocks: [
+  MockSpec<BinaryReader>(returnNullOnMissingStub: true),
+  MockSpec<BinaryWriter>(returnNullOnMissingStub: true),
+])
 // ignore: prefer_typing_uninitialized_variables, unused_element
 var _mocks;
