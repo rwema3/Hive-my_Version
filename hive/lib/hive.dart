4d2255a8c794a19de6dd110f97fe83b893ecfe7e
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-06 20:45:27 +0200

    import

--- hive/lib/hive.dart
+++ hive/lib/hive.dart
@@ -11,3 +11,4 @@ import 'package:crypto/crypto.dart';
 import 'package:hive/src/backend/storage_backend.dart';
 import 'package:hive/src/box/default_compaction_strategy.dart';
 import 'package:hive/src/box/default_key_comparator.dart';
+import 'package:hive/src/crypto/aes_cbc_pkcs7.dart';
