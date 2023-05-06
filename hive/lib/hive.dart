0c611031b48dd5aab39969ad59a10905da408407
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-06 20:45:47 +0200

    hive for crc32

--- hive/lib/hive.dart
+++ hive/lib/hive.dart
@@ -12,3 +12,5 @@ import 'package:hive/src/backend/storage_backend.dart';
 import 'package:hive/src/box/default_compaction_strategy.dart';
 import 'package:hive/src/box/default_key_comparator.dart';
 import 'package:hive/src/crypto/aes_cbc_pkcs7.dart';
+import 'package:hive/src/crypto/crc32.dart';
+import 'package:hive/src/hive_impl.dart';
