304787db2a22e62eb28a3423f294569f074d2c45
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-07 01:20:48 +0200

    annotations

--- hive_flutter/test/mocks.dart
+++ hive_flutter/test/mocks.dart
@@ -1,5 +1,9 @@
 library hive_flutter.test.mocks;
 
+import 'package:hive/hive.dart';
+import 'package:mockito/annotations.dart';
+
+export 'mocks.mocks.dart';
 
 @GenerateMocks([], customMocks: [
   MockSpec<BinaryReader>(returnNullOnMissingStub: true),
